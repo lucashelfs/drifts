475c880eca5c1cd79e17245eb83991e698028538
Lucas Helfstein <lucas.helfstein@idwall.co>
2023-12-04 22:36:22 -0300

    chore: add utils file

--- dev/codes/utils.py
+++ dev/codes/utils.py
@@ -1,8 +1,11 @@
 import json
+from usp_stream_datasets import load_insect_dataset, insects_datasets
 
 
-def generate_filenames(dataset: str, results_folder: str = "results/"):
-    """Util for experiment filenames."""
+def generate_experiment_filenames(
+    dataset: str, results_folder: str = "results/"
+):
+    """Generates experiment filenames."""
     dataset_prefix = results_folder + dataset.lower().replace(".", "").replace(
         "(", ""
     ).replace(")", "").replace(" ", "-")
@@ -10,6 +13,13 @@ def generate_filenames(dataset: str, results_folder: str = "results/"):
     metadata_file = dataset_prefix + "_metadata.json"
     plot_file = dataset_prefix + ".jpg"
     return csv_file, metadata_file, plot_file
+
+
+def fetch_original_dataframe(dataset_identifier):
+    """Fetches the original dataframe given the identifier from
+    usp stream datasets.
+    """
+    return load_insect_dataset(insects_datasets[dataset_identifier]["filename"])
 
 
 def open_metadata_file(filename):
