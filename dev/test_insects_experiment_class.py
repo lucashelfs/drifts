cfc2a720d1ab3e5f3e7a3fd0e72f7080d9b646dc
Lucas Helfstein <lucas.helfstein@idwall.co>
2024-03-09 13:42:06 -0300

    refactor: wip refactor visualizer oriented to experiments

--- dev/test_insects_experiment_class.py
+++ dev/test_insects_experiment_class.py
@@ -1,5 +1,6 @@
 import os
 from codes.insects_experiment import InsectsExperiment
+from codes.visualizer import InsectsVisualizer
 
 n_bins = 5
 dataset_name = "Incremental (bal.)"
@@ -11,11 +12,9 @@ median_origin = "both"
 results_folder = os.getcwd() + f"/TEST-results_of_{test_type}_{n_bins}_{attr}/"
 config = dict(
     batches=False,
-    dataset_name=dataset_name,
     dataset=dataset_name,
     attr=attr,
     results_folder=results_folder,
-    data_source="insects",
     test_type=test_type,
     n_bins=n_bins,
     binning_type=binning_type,
@@ -24,14 +23,16 @@ config = dict(
 
 
 ie = InsectsExperiment(**config)
-ie.run_test()
+# ie.run_test()
 
 
 # # Generate GIF of stream binning
 # # create_binning_timeline(exp.df_baseline, exp.df_stream, **config)
 
 # # # Plot values for that attribute
-# # plot_kl_values(**config)
+iv = InsectsVisualizer(ie)
+iv.plot_kl_values(**config)
+
 
 # # TODO: improve below
 # # Plot first baseline bin
