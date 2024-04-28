5672a5f17b6c7931ea02124838e981a833a75515
Lucas Helfstein <lucashelfstein@gmail.com>
2024-04-28 12:26:10 -0300

    feat: testing template files

--- dev/test_insects_experiment_class.py
+++ dev/test_insects_experiment_class.py
@@ -1,5 +1,9 @@
 import os
+import numpy as np
 from codes.insects_experiment import InsectsExperiment, InsectsVisualizer
+from codes.binning_visualizer import create_binning_timeline
+
+from codes.usp_stream_datasets import insects_datasets
 
 
 if __name__ == "__main__":
@@ -7,11 +11,44 @@ if __name__ == "__main__":
     n_bins = 5
     dataset_name = "Incremental (bal.)"
     attr = "Att31"
-    test_type = "ks"
-    binning_type = "median"
-    median_origin = "both"
+    # window_size = 1000
+    window_size = None
+    corrected = True
+    test_type = "kl"
+    binning_type = "dummy"
+    bins_origin = "baseline"
 
-    results_folder = os.getcwd() + f"/TEST-results_of_{test_type}_{n_bins}_{attr}/"
+    # When loooping for all...
+    # for dataset_name in insects_datasets.keys():
+    # KL/KS
+    # test_types = ["kl", "ks"]
+    # binning_types = ["dummy"]
+    # bins_origins = ["baseline"]
+
+    # for dataset_name in insects_datasets.keys():
+
+    #     if dataset_name != "Incremental (bal.)":
+
+    dataset_filename_str = (
+        dataset_name.lower()
+        .replace(".", "")
+        .replace("(", "")
+        .replace(")", "")
+        .replace(" ", "_")
+    )
+
+    results_folder = (
+        os.getcwd()
+        + f"/results_insects/{dataset_filename_str}_{test_type}_{binning_type}_{n_bins}"
+    )
+
+    if window_size:
+        results_folder += f"_window_size_{window_size}"
+
+    if attr:
+        results_folder += f"_{attr}"
+
+    results_folder += "/"
 
     config = dict(
         batches=False,
@@ -21,23 +58,52 @@ if __name__ == "__main__":
         test_type=test_type,
         n_bins=n_bins,
         binning_type=binning_type,
-        median_origin=median_origin,
+        bins_origin=bins_origin,
+        window_size=window_size,
     )
 
-    # Set the experiment
     ie = InsectsExperiment(**config)
 
     # Run the experiment
     ie.run_test()
 
-    # Set a visualizer for the experiment result
-    iv = InsectsVisualizer(ie)
+    # # TODO: fetch the number of bins in a dynamic way
+    # # Set the experiment just for fetching the default window size
+    # default_dataset_window_size = ie.window_size
 
-    # Plot the obtained results
-    iv.plot_result_values(**config)
+    # n_bins_list = [x for x in range(5, int(np.sqrt(default_dataset_window_size)), 5)]
+    # if int(np.sqrt(window_size)) not in n_bins_list:
+    #     n_bins_list.append(int(np.sqrt(window_size)))
+
+    # # ALL THE FORS GO HERE
+    # # fetch new config and run...
+
+    # config = dict(
+    #     batches=False,
+    #     dataset=dataset_name,
+    #     # attr=attr,
+    #     results_folder=results_folder,
+    #     test_type=test_type,
+    #     n_bins=n_bins,
+    #     binning_type=binning_type,
+    #     bins_origin=bins_origin,
+    #     window_size=window_size,
+    # )
+
+    # # Set the experiment just for fetching the default window size
+    # ie = InsectsExperiment(**config)
+
+    # # Run the experiment
+    # ie.run_test()
+
+    # # Set a visualizer for the experiment result
+    # iv = InsectsVisualizer(ie)
+
+    # # # Plot the obtained results
+    # iv.plot_result_values(**config)
 
     # # Plot original data for the attr for example
     # iv.plot_original_data(**config)
 
-    # Generate GIF of stream binning
-    # create_binning_timeline(exp.df_baseline, exp.df_stream, **config)
+    # Generate GIF of stream binning: how do this works for each attr??
+    # create_binning_timeline(ie.df_baseline, ie.df_stream, **config)
--- dev/test_synthetic_experiment.py
+++ dev/test_synthetic_experiment.py
@@ -1,19 +1,14 @@
 import os
 from codes.synthetic_experiment import SyntheticExperiment, SynthethicVisualizer
+from codes.binning_visualizer import create_binning_timeline
 
 import numpy as np
 
-
-# Synthetic data params
-random_state = np.random.RandomState(seed=42)
-visualize_stream = True
-synthetic_params = [
-    random_state.normal(0.8, 0.05, 2000),
-    random_state.normal(0.7, 0.05, 2000),
-    random_state.normal(0.6, 0.1, 2000),
-]
-
-window_size = 1000
+# synthetic_params = [
+#     random_state.normal(0.5, 0.05, 1500),
+#     random_state.normal(0.5, 0.1, 1000),
+#     random_state.normal(0.5, 0.2, 1000),
+# ]
 
 ## KS config
 # test_type = "ks"
@@ -27,22 +22,79 @@ window_size = 1000
 #     window_size=window_size,
 # )
 
-# JS/KL config
-test_type = "js_dummy"
-n_bins = 5
-results_folder = os.getcwd() + f"/TEST-SYNTHETIC-results_of_{test_type}/"
 
-config = dict(
-    batches=False,
-    results_folder=results_folder,
-    test_type=test_type,
-    window_size=window_size,
-    n_bins=n_bins,
-    visualize_stream=visualize_stream,
-    distributions=synthetic_params,
-)
+# Synthetic data params
+random_state = np.random.RandomState(seed=42)
+visualize_stream = True
+
+synthetic_params = [
+    random_state.normal(0.5, 0.1, 2500),
+    random_state.normal(0.7, 0.15, 2000),
+    random_state.normal(0.9, 0.2, 2000),
+    # random_state.normal(0.7, 0.15, 2000),
+    # random_state.normal(0.5, 0.2, 2500),
+    # random_state.normal(0.5, 0.1, 2500),
+]
+
 
 if __name__ == "__main__":
+
+    baseline_size = 500
+    window_size = 500
+
+    # JS/KL/Hellinger config
+    test_types = ["kl", "js", "hellinger"]
+    binning_types = ["dummy", "median"]
+    bins_origins = ["baseline", "stream", "both"]
+
+    corrected = True
+
+    # n_bins_list = [5]
+    n_bins_list = [x for x in range(5, int(np.sqrt(window_size)), 5)]
+    if int(np.sqrt(window_size)) not in n_bins_list:
+        n_bins_list.append(int(np.sqrt(window_size)))
+
+    ## Loop for all binning based...
+    # for test_type in test_types:
+    #     for binning_type in binning_types:
+    #         for bins_origin in bins_origins:
+    #             for n_bins in n_bins_list:
+
+    # Single run for KS
+    test_type = "ks"
+    n_bins = 0
+    binning_type = False
+    bins_origin = None
+
+    folder_name = f"/results_synthetic/base_{test_type}"
+
+    if test_type != "ks":
+        folder_name += f"_{n_bins}_bins_{binning_type}_{bins_origin}"
+
+    if not corrected:
+        folder_name += "_uncorrected/"
+    else:
+        folder_name += "/"
+
+    results_folder = os.getcwd() + folder_name
+
+    config = dict(
+        batches=False,
+        results_folder=results_folder,
+        test_type=test_type,
+        window_size=window_size,
+        baseline_size=baseline_size,
+        n_bins=n_bins,
+        visualize_stream=visualize_stream,
+        distributions=synthetic_params,
+        bins_origin=bins_origin,
+        binning_type=binning_type,
+        gif_frames_step=250,
+        corrected=corrected,
+        savefig=True,
+        displayfig=False,
+    )
+
     # Set the experiment
     se = SyntheticExperiment(**config)
     se.run_test()
@@ -52,3 +104,17 @@ if __name__ == "__main__":
 
     # Plot the obtained results
     sv.plot_result_values(**config)
+
+    # Create gif
+    _, _, _, binning_timeline_filename = sv.fetch_experiment_filename()
+
+    # TODO: incorporate the binning timeline on the base visualizer in the future.
+    # TODO: fixar o eixo X
+    # TODO: check gifs from median and both
+    if test_type != "ks":
+        create_binning_timeline(
+            se.df_baseline,
+            se.df_stream,
+            filename=binning_timeline_filename,
+            **config,
+        )
